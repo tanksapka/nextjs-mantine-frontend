8e26ea40c7375516a7b18a5054634515bf6ab1fb
Tamas Schell <tanksapka@yahoo.com>
2022-05-21 23:45:47 +0200

    Adding simplified version of react table component for testing.

---
+++ hoc/mantine-table/SimpleTable.tsx
@@ -0,0 +1,48 @@
+import { useMemo } from "react";
+import { Column, useTable } from "react-table";
+
+function SimpleTable({ columns, data }: { columns: Array<Column>; data: Array<any> }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => {
+          const { key, ...restHeaderGroupProps } = headerGroup.getHeaderGroupProps();
+          return (
+            <tr key={key} {...restHeaderGroupProps}>
+              {headerGroup.headers.map((column) => {
+                const { key, ...restColumn } = column.getHeaderProps();
+                return (
+                  <th key={key} {...restColumn}>
+                    {column.render("Header")}
+                  </th>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </thead>
+      <tbody {...getTableBodyProps}>
+        {rows.map((row) => {
+          prepareRow(row);
+          const { key, ...restRowProps } = row.getRowProps();
+          return (
+            <tr key={key} {...restRowProps}>
+              {row.cells.map((cell) => {
+                const { key, ...restCellProps } = cell.getCellProps();
+                return (
+                  <td key={key} {...restCellProps}>
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export { SimpleTable };
