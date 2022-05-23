08948ba949189bc9ee7cceb40c59015d0cf410ea
Tamas Schell <tanksapka@yahoo.com>
2022-05-23 23:59:35 +0200

    Adding pagination and types.

--- hoc/mantine-table/SimpleTable.tsx
+++ hoc/mantine-table/SimpleTable.tsx
@@ -1,70 +1,110 @@
-import { Table } from "@mantine/core";
+import { Center, Group, Pagination, Select, Table } from "@mantine/core";
 import { IconArrowsSort, IconSortAscending, IconSortDescending } from "@tabler/icons";
-import { Column, useTable, useSortBy } from "react-table";
+import { Column, useTable, useSortBy, usePagination } from "react-table";
+
+const pageSizeOptions = [
+  { value: "10", label: "10" },
+  { value: "20", label: "20" },
+  { value: "50", label: "50" },
+  { value: "100", label: "100" },
+];
 
 function SimpleTable({ columns, data }: { columns: Array<Column>; data: Array<any> }) {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    pageCount,
+    gotoPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+    prepareRow,
+  } = useTable(
     {
       columns,
       data,
-      // initialState: {
-      //   hiddenColumns: columns
-      //     .filter((column) => column.accessor?.toString().endsWith("id"))
-      //     .map((col): string => (col.accessor ? col.accessor?.toString() : "")),
-      // },
+      initialState: {
+        pageSize: 20,
+        //   hiddenColumns: columns
+        //     .filter((column) => column.accessor?.toString().endsWith("id"))
+        //     .map((col): string => (col.accessor ? col.accessor?.toString() : "")),
+      },
     },
-    useSortBy
+    useSortBy,
+    usePagination
   );
 
   return (
-    <Table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => {
-          const { key, ...restHeaderGroupProps } = headerGroup.getHeaderGroupProps();
-          return (
-            <tr key={key} {...restHeaderGroupProps}>
-              {headerGroup.headers.map((column) => {
-                const { key, ...restColumn } = column.getHeaderProps(column.getSortByToggleProps());
-                return (
-                  <th key={key} {...restColumn} hidden={!column.isVisible}>
-                    {column.render("Header")}
-                    <span style={{ marginLeft: "1rem" }}>
-                      {column.isSorted ? (
-                        column.isSortedDesc ? (
-                          <IconSortDescending size={16} />
+    <>
+      <Table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => {
+            const { key, ...restHeaderGroupProps } = headerGroup.getHeaderGroupProps();
+            return (
+              <tr key={key} {...restHeaderGroupProps}>
+                {headerGroup.headers.map((column) => {
+                  const { key, ...restColumn } = column.getHeaderProps(column.getSortByToggleProps());
+                  return (
+                    <th key={key} {...restColumn} hidden={!column.isVisible}>
+                      {column.render("Header")}
+                      <span style={{ marginLeft: "1rem" }}>
+                        {column.isSorted ? (
+                          column.isSortedDesc ? (
+                            <IconSortDescending size={16} />
+                          ) : (
+                            <IconSortAscending size={16} />
+                          )
                         ) : (
-                          <IconSortAscending size={16} />
-                        )
-                      ) : (
-                        <IconArrowsSort size={16} />
-                      )}
-                    </span>
-                  </th>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </thead>
-      <tbody {...getTableBodyProps}>
-        {rows.map((row) => {
-          prepareRow(row);
-          const { key, ...restRowProps } = row.getRowProps();
-          return (
-            <tr key={key} {...restRowProps}>
-              {row.cells.map((cell) => {
-                const { key, ...restCellProps } = cell.getCellProps();
-                return (
-                  <td key={key} {...restCellProps}>
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+                          <IconArrowsSort size={16} />
+                        )}
+                      </span>
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </thead>
+        <tbody {...getTableBodyProps}>
+          {page.map((row) => {
+            prepareRow(row);
+            const { key, ...restRowProps } = row.getRowProps();
+            return (
+              <tr key={key} {...restRowProps}>
+                {row.cells.map((cell) => {
+                  const { key, ...restCellProps } = cell.getCellProps();
+                  return (
+                    <td key={key} {...restCellProps}>
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        {/* <Center> */}
+        <Pagination
+          onChange={(n: number) => gotoPage(n - 1)}
+          mt="lg"
+          page={pageIndex + 1}
+          style={{ justifyContent: "center" }}
+          total={pageCount}
+          withEdges
+        />
+        {/* </Center> */}
+        <Select
+          mt="lg"
+          ml="sm"
+          data={pageSizeOptions}
+          defaultValue={pageSize.toString()}
+          onChange={(value) => setPageSize(parseInt(value as string))}
+        />
+      </div>
+    </>
   );
 }
 
--- pages/organizations/index.tsx
+++ pages/organizations/index.tsx
@@ -1,21 +1,11 @@
-import { Button, Group, Pagination } from "@mantine/core";
+import { Button, Group } from "@mantine/core";
 import { IconPlus } from "@tabler/icons";
 import { GetServerSideProps } from "next";
-import { useState } from "react";
 import { Organizations } from "../../components/organizations/Organizations";
 import { OrganizationsRawType } from "../../types/organizations";
 import { getOrganizations } from "../../utils/organizations";
 
 function OrganizationsIndex(organziationData: OrganizationsRawType) {
-  const [data, setData] = useState(organziationData);
-  const [activePage, setPage] = useState(organziationData.page + 1);
-
-  async function changeHandler(page: number) {
-    const res = await getOrganizations(page - 1, organziationData.page_size);
-    setPage(page);
-    setData(res);
-  }
-
   return (
     <>
       <Group mb="lg" position="right">
@@ -23,15 +13,7 @@ function OrganizationsIndex(organziationData: OrganizationsRawType) {
           Új szervezet
         </Button>
       </Group>
-      <Organizations {...data} />
-      <Pagination
-        onChange={changeHandler}
-        mt="lg"
-        page={activePage}
-        style={{ justifyContent: "center" }}
-        total={organziationData.page_count}
-        withEdges
-      />
+      <Organizations {...organziationData} />
     </>
   );
 }
@@ -39,7 +21,7 @@ function OrganizationsIndex(organziationData: OrganizationsRawType) {
 export default OrganizationsIndex;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const organziationData = await getOrganizations(0, 20);
+  const organziationData = await getOrganizations(0, 60);
 
   if (!organziationData) {
     return {
