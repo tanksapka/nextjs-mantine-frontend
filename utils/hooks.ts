eee26ec2a82d6e284fb2ba90e8c0f63039153c96
Tamas Schell <tanksapka@yahoo.com>
2022-07-18 23:55:48 +0200

    Adding initial version of useFormCustom to handle changing values within form.

--- utils/hooks.ts
+++ utils/hooks.ts
@@ -1,5 +1,22 @@
-import { SyntheticEvent, useEffect } from "react";
+import { useEffect } from "react";
 import Router from "next/router";
+import { UseFormInput, UseFormReturnType } from "@mantine/form/lib/use-form";
+import { useForm } from "@mantine/form";
+import { isDeepStrictEqual } from "util";
+
+export function useFormCustom<Values = Record<string, unknown>>({
+  initialValues = {} as Values,
+  initialErrors = {},
+}: // clearInputErrorOnChange = true,
+// validateInputOnChange = false,
+// validate: rules,
+UseFormInput<Values>): any {
+  const form = useForm({ initialValues, initialErrors });
+
+  const isDirty = () => isDeepStrictEqual(form.values, initialValues) === false;
+
+  return { ...form, isDirty };
+}
 
 export function useWarnIfUnsavedChanges(unsavedChanges: boolean) {
   const message = "Do you want to leave?";
