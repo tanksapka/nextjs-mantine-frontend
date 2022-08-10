8e43b351f69d75f8ca0a1714c7d2841c710927ba
Tamas Schell <tanksapka@yahoo.com>
2022-08-10 23:55:10 +0200

    Starting migration to react-table 8.

--- components/organizations/Organizations.tsx
+++ components/organizations/Organizations.tsx
@@ -3,64 +3,94 @@ import { IconPencil } from "@tabler/icons";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useMemo } from "react";
-import { Column, Row } from "react-table";
+import { createColumnHelper, Column, Row } from "@tanstack/react-table";
 import { DateFilter, StringFilter } from "../../hoc/mantine-table/filters";
 import filterObject from "../../hoc/mantine-table/filterTypes";
 import { SimpleTable } from "../../hoc/mantine-table/SimpleTable";
 import { OrganizationsRawType, OrganizationsRowItem } from "../../types/organizations";
 
 function Organizations({ organizations, page_count, page_size, page }: OrganizationsRawType) {
-  const columns = useMemo(
-    (): Array<Column> => [
-      {
-        Header: "Alapszervezet azonosítója",
-        accessor: "organization_id",
-      },
-      {
-        Header: "Alapszervezet neve",
-        accessor: "organization_name",
-        Filter: StringFilter,
-        filter: filterObject.stringFilter,
-      },
-      {
-        Header: "Szülőszervezet azonosítója",
-        accessor: "parent_organization_id",
-      },
-      {
-        Header: "Szülőszervezet neve",
-        accessor: "parent_organization_name",
-        Filter: StringFilter,
-        filter: filterObject.stringFilter,
-      },
-      {
-        Header: "Leírás",
-        accessor: "description",
-      },
-      {
-        Header: "Fogad tagokat?",
-        accessor: "accepts_members_flag",
-        Filter: StringFilter,
-        filter: filterObject.stringFilter,
-      },
-      {
-        Header: "Alapítás dátuma",
-        accessor: "establishment_date",
-        Filter: DateFilter,
-        filter: filterObject.dateFilter,
-      },
-      {
-        Header: "Megszűnés dátuma",
-        accessor: "termination_date",
-        Filter: DateFilter,
-        filter: filterObject.dateFilter,
-      },
-      {
-        Header: "Jegyzetek",
-        accessor: "notes",
-      },
-    ],
-    []
-  );
+  const columnHelper = createColumnHelper<OrganizationsRowItem>();
+  const columns = [
+    columnHelper.accessor("organization_id", {
+      header: "Alapszervezet azonosítója",
+    }),
+    columnHelper.accessor("organization_name", {
+      header: "Alapszervezet neve",
+    }),
+    columnHelper.accessor("parent_organization_id", {
+      header: "Szülőszervezet azonosítója",
+    }),
+    columnHelper.accessor("parent_organization_name", {
+      header: "Szülőszervezet neve",
+    }),
+    columnHelper.accessor("description", {
+      header: "Leírás",
+    }),
+    columnHelper.accessor("accepts_members_flag", {
+      header: "Fogad tagokat?",
+    }),
+    columnHelper.accessor("establishment_date", {
+      header: "Alapítás dátuma",
+    }),
+    columnHelper.accessor("termination_date", {
+      header: "Megszűnés dátuma",
+    }),
+    columnHelper.accessor("notes", {
+      header: "Jegyzetek",
+    }),
+  ];
+  // const columns = useMemo(
+  //   (): Array<Column> => [
+  //     {
+  //       Header: "Alapszervezet azonosítója",
+  //       accessor: "organization_id",
+  //     },
+  //     {
+  //       Header: "Alapszervezet neve",
+  //       accessor: "organization_name",
+  //       Filter: StringFilter,
+  //       filter: filterObject.stringFilter,
+  //     },
+  //     {
+  //       Header: "Szülőszervezet azonosítója",
+  //       accessor: "parent_organization_id",
+  //     },
+  //     {
+  //       Header: "Szülőszervezet neve",
+  //       accessor: "parent_organization_name",
+  //       Filter: StringFilter,
+  //       filter: filterObject.stringFilter,
+  //     },
+  //     {
+  //       Header: "Leírás",
+  //       accessor: "description",
+  //     },
+  //     {
+  //       Header: "Fogad tagokat?",
+  //       accessor: "accepts_members_flag",
+  //       Filter: StringFilter,
+  //       filter: filterObject.stringFilter,
+  //     },
+  //     {
+  //       Header: "Alapítás dátuma",
+  //       accessor: "establishment_date",
+  //       Filter: DateFilter,
+  //       filter: filterObject.dateFilter,
+  //     },
+  //     {
+  //       Header: "Megszűnés dátuma",
+  //       accessor: "termination_date",
+  //       Filter: DateFilter,
+  //       filter: filterObject.dateFilter,
+  //     },
+  //     {
+  //       Header: "Jegyzetek",
+  //       accessor: "notes",
+  //     },
+  //   ],
+  //   []
+  // );
   const data = useMemo(() => organizations, []);
   const router = useRouter();
 
